a96fff75e506e2647452d6fe13ed74605a5eebff
Ethan Richards <42894274+ezrichards@users.noreply.github.com>
2023-06-08 09:28:52 -0600

    Implement custom passport strategy with MPAPI

--- app.js
+++ app.js
@@ -1,17 +1,84 @@
 const express = require('express')
-const path = require('path');
 const ejsMate = require('ejs-mate');
+const session = require('express-session');
+const path = require('path');
+const passport = require('passport');
+const CustomStrategy = require('passport-custom').Strategy;
+const axios = require('axios')
 const app = express()
 const port = 3000
 
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
-
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-  res.render('home', { title: 'Mines ACM' })
+const sessionConfig = {
+  name: 'session',
+  secret: 'yeehaw',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+      httpOnly: true,
+      // secure: true,
+      expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+      maxAge: 1000 * 60 * 60 * 24 * 7
+  }
+}
+
+app.use(session(sessionConfig));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use('mpapi', new CustomStrategy(
+  async function(req, callback) {
+    let userData = {};
+    if(req.query['tkt'] != undefined) {
+      await axios.post('https://mastergo.mines.edu/mpapi/fetch?tkt=' + req.query['tkt']).then(function(response) {
+        // TODO this can probably be organized/added better
+        userData['uid'] = response.data['uid'];
+        userData['mail'] = response.data.attributes['mail'];
+        userData['first'] = response.data.attributes['first'];
+        userData['last'] = response.data.attributes['sn'];
+        userData['full'] = response.data.attributes['gecos'];
+      }).catch(function (error) {
+        if(error.response.status == 500) {
+          userData['uid'] = 'session expired'
+        }
+      });
+    }
+
+    console.log("got back:", userData)
+    callback(null, userData);
+  }
+));
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+
+app.use(function(req,res,next){
+  res.locals.currentUser = req.user;
+  next();
+})
+
+app.get('/', passport.authenticate('mpapi', { failureRedirect: '/' }), async(req, res) => {
+  if(req.user.uid == 'session expired') {
+    console.log('REDIRECTING currentuser:', req.user)
+    res.redirect('/')
+  }
+  else {
+    if(req.isAuthenticated()) {
+      res.render('home', { title: 'Mines ACM', user: req.user })
+    }
+    else {
+      res.render('home', { title: 'Mines ACM' })
+    }
+  }
 })
 
 const people = [
@@ -56,7 +123,7 @@ const people = [
 ]
 
 app.get('/about', (req, res) => {
-  res.render('about', { title: 'About Us | Mines ACM', people })
+  res.render('about', { title: 'About Us | Mines ACM', people, user: false })
 })
 
 const presentations = [
@@ -67,6 +134,13 @@ const presentations = [
   }
 ]
 
+app.get('/login', (req, res) => {
+  console.log("Came from:", req.url)
+
+
+  res.redirect('https://mastergo.mines.edu/mpapi/sso?return=http://localhost:3000/')
+})
+
 app.get('/presentations', (req, res) => {
   res.render('presentations', { title: 'Presentations | Mines ACM', presentations })
 })
@@ -75,9 +149,13 @@ app.get('/projects', (req, res) => {
   res.render('projects', { title: "Projects | Mines ACM"})
 })
 
-// app.get('/admim', (req, res) => {
-//   res.render('admin')
-// })
+app.get('/profile', (req, res) => {
+  res.render('profile')
+})
+
+app.get('/admim', (req, res) => {
+  res.render('admin')
+})
 
 app.use((req, res, next) => {
   res.status(404).render('404', { title: "404 | Mines ACM"});
