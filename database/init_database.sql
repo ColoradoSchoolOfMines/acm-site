1e8b3b18617eb2cfcefc4c094b642fa4751d4c1b
Ethan Richards <42894274+ezrichards@users.noreply.github.com>
2023-06-11 07:06:00 -0600

    db: Swap meeting-survey foreign key

--- database/init_database.sql
+++ database/init_database.sql
@@ -3,29 +3,8 @@ CREATE TABLE IF NOT EXISTS users (
     email      TEXT NOT NULL PRIMARY KEY,
     first_name TEXT NOT NULL,
     last_name  TEXT NOT NULL,
-    is_officer BOOLEAN DEFAULT FALSE,
     title      TEXT,
     picture    TEXT
-);
-
-CREATE TABLE IF NOT EXISTS surveys (
-    id              INTEGER PRIMARY KEY,
-    opens           TIMESTAMP,
-    closes          TIMESTAMP,
-    allow_anonymous BOOLEAN DEFAULT TRUE
-);
-
--- CREATE TABLE IF NOT EXISTS survey_fields(
-
--- );
-
--- CREATE TABLE IF NOT EXISTS survey_responses(
-
--- );
-
-CREATE TABLE IF NOT EXISTS images(
-    "url"   TEXT NOT NULL,
-    caption TEXT
 );
 
 CREATE TABLE IF NOT EXISTS meetings (
@@ -35,8 +14,29 @@ CREATE TABLE IF NOT EXISTS meetings (
     title         TEXT NOT NULL,
     "description" TEXT,
     "type"        TEXT,
-    id            INTEGER PRIMARY KEY,
-    survey INT REFERENCES surveys(id)
+    id            INTEGER PRIMARY KEY
+);
+
+CREATE TABLE IF NOT EXISTS surveys (
+    id              INTEGER PRIMARY KEY,
+    opens           TIMESTAMP,
+    closes          TIMESTAMP,
+    allow_anonymous BOOLEAN DEFAULT TRUE,
+    meeting         INT REFERENCES meetings(id)
+);
+
+-- CREATE TABLE IF NOT EXISTS survey_fields(
+
+-- );
+
+-- CREATE TABLE IF NOT EXISTS survey_responses(
+
+--     response TEXT REFERENCES users(email)
+-- );
+
+CREATE TABLE IF NOT EXISTS images(
+    "url"   TEXT NOT NULL,
+    caption TEXT
 );
 
 CREATE TABLE IF NOT EXISTS presentations (
