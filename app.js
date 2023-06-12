741522948eeceb632ec018bbdca7e34ae4fbd29e
Ethan Richards <42894274+ezrichards@users.noreply.github.com>
2023-06-12 07:43:02 -0600

    Use dynamic URL for callback

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use(passport.session());
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/auth/google/callback",
+  callbackURL: "/auth/google/callback",
   passReqToCallback: true
 }, (req, accessToken, refreshToken, profile, done) => {
   if (profile.email.endsWith("@mines.edu")) {
