fcf1988d6b35e92654f3290e1cfbc370debf4c91
Alexander Capehart <alex@oxycblt.org>
2023-06-16 11:45:22 -0600

    Validate uploaded images
    Validate that images uploaded are valid files, and convert them to PNG
    to ensure maximum device compatibility.
    
    Errors thrown currently aren't the best, that will be worked on later.

---
+++ imagestorage.js
@@ -0,0 +1,38 @@
+var fs = require('fs')
+var sharp = require("sharp")
+var uuid = require("uuid")
+
+function getDestination(req, file, cb) {
+    cb(null, '/dev/null')
+}
+
+function ImageStorage() { }
+
+ImageStorage.prototype._handleFile = function _handleFile(req, file, cb) {
+    file.filename = uuid.v4()
+    const path = "uploads/" + file.filename
+
+    var pngConverter = sharp().toFormat('png')
+    pngConverter.on('error', cb)
+
+    var outStream = fs.createWriteStream(path)
+    outStream.on('error', cb)
+    outStream.on('finish', function () {
+        cb(null, {
+            path: path,
+            size: outStream.bytesWritten
+        })
+    })
+
+    file.stream
+        .pipe(pngConverter)
+        .pipe(outStream)
+}
+
+ImageStorage.prototype._removeFile = function _removeFile(req, file, cb) {
+    fs.unlink(file.path, cb)
+}
+
+module.exports = function (opts) {
+    return new ImageStorage(opts)
+}
