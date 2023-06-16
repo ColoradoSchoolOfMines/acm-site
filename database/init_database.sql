98fffbe72c5b93b37efbc52d164d4e149c59f13b
Ethan Richards <42894274+ezrichards@users.noreply.github.com>
2023-06-16 13:56:07 -0600

    Update images table and use consistent string naming

--- database/init_database.sql
+++ database/init_database.sql
@@ -1,54 +1,51 @@
--- Initialize all tables with defaults
-
 CREATE TABLE IF NOT EXISTS users (
-    email      TEXT NOT NULL PRIMARY KEY,
-    first_name TEXT NOT NULL,
-    last_name  TEXT NOT NULL,
-    title      TEXT,
-    picture_id TEXT
+    "email"      TEXT NOT NULL PRIMARY KEY,
+    "first_name" TEXT NOT NULL,
+    "last_name"  TEXT NOT NULL,
+    "title"      TEXT,
+    "avatar_id"  TEXT
 );
 
 CREATE TABLE IF NOT EXISTS projects (
-    id            INTEGER PRIMARY KEY,
-    title         TEXT NOT NULL,
-    "description" TEXT,
-    website       TEXT,
-    repository    TEXT,
-    archived      BOOLEAN DEFAULT FALSE
+    "id"           INTEGER PRIMARY KEY,
+    "title"        TEXT NOT NULL,
+    "description"  TEXT,
+    "website"      TEXT,
+    "repository"   TEXT,
+    "archived"     BOOLEAN DEFAULT FALSE
 );
 
 CREATE TABLE IF NOT EXISTS user_projects (
-    user_id    TEXT REFERENCES users(email),
-    project_id INTEGER REFERENCES projects(id),
+    "user_id"    TEXT REFERENCES users(email),
+    "project_id" INTEGER REFERENCES projects(id),
     PRIMARY KEY (user_id, project_id)
 );
 
 CREATE TABLE IF NOT EXISTS meetings (
-    "date"        TIMESTAMP,
-    duration      INTEGER,
-    "location"    TEXT,
-    title         TEXT NOT NULL,
+    "id"          INTEGER PRIMARY KEY
+    "title"       TEXT NOT NULL,
     "description" TEXT,
+    "date"        TIMESTAMP,
+    "duration"    INTEGER,
+    "location"    TEXT,
     "type"        TEXT,
-    id            INTEGER PRIMARY KEY
 );
 
 CREATE TABLE IF NOT EXISTS attendance (
-    meeting  INTEGER PRIMARY KEY REFERENCES meetings(id),
-    opens    TIMESTAMP,
-    closes   TIMESTAMP,
-    "user"   TEXT REFERENCES users(email)
+    "meeting" INTEGER PRIMARY KEY REFERENCES meetings(id),
+    "opens"   TIMESTAMP,
+    "closes"  TIMESTAMP,
+    "user"    TEXT REFERENCES users(email)
 );
 
 CREATE TABLE IF NOT EXISTS images (
-    id        TEXT PRIMARY KEY,
-    caption   TEXT,
-    "profile" BOOLEAN
+    "id"       TEXT PRIMARY KEY,
+    "caption"  TEXT
 );
 
 CREATE TABLE IF NOT EXISTS presentations (
-    id            INTEGER PRIMARY KEY,
-    title         TEXT NOT NULL,
+    "id"          INTEGER PRIMARY KEY,
+    "title"       TEXT NOT NULL,
     "description" TEXT,
     "date"        TIMESTAMP,
     "url"         TEXT
