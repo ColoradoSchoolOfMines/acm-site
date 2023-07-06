f458c057b256fc24bf4b60f49663b9fe9b359307
Alexander Capehart <alex@oxycblt.org>
2023-07-06 13:18:32 -0600

    Add project editing backend
    Add the backend POST handler for project editing, which largely
    completes this functionality.

--- routes/projects.js
+++ routes/projects.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const db = require('../database/db');
+const fs = require('fs')
 const { isAdminAuthenticated, upload } = require('../middleware');
 const router = express.Router();
+
+const getDBArchivedValue = (req) => (req.body.archived !== undefined).toString()
 
 router.get('/projects', async (req, res) => {
   const resp = await db.query("SELECT * FROM projects ORDER BY archived, title");
@@ -12,8 +15,31 @@ router.post('/projects', isAdminAuthenticated, upload('image'), async (req, res)
   await db.query(
     "INSERT INTO projects VALUES ($1, $2, $3, $4, $5, $6, $7)",
     [uuid.v4(), req.body.title, req.body.description, req.body.website, req.body.repository, 
-      (req.body.archived !== undefined).toString(), req.file.filename]);
+      getDBArchivedValue(req), req.file.filename]);
   req.flash('success', 'Successfully added project!');
+  res.redirect('/projects');
+});
+
+router.post('/projects/edit', isAdminAuthenticated, upload('image', true), async (req, res) => {
+  // Image uploading is optional when editing projects, so we need to perform different queries
+  // for the different states.
+  if (req.file) {
+      // Free the space taken up by the now-unused project image. This requires us to obtain
+      // the original image ID from the database.
+      let resp = await db.query("SELECT image_id FROM projects WHERE id = $1", [req.body.id]);
+      fs.unlinkSync("uploads/" + resp.rows[0].image_id);
+      await db.query(
+        "UPDATE projects SET title = $1, description = $2, website = $3, repository = $4, archived = $5, image_id = $6 WHERE id = $7",
+        [req.body.title, req.body.description, req.body.website, req.body.repository, 
+          getDBArchivedValue(req), req.file.filename, req.body.id])
+  } else {
+    // No image specified, leave it unchanged and only commit the rest.
+    await db.query(
+      "UPDATE projects SET title = $1, description = $2, website = $3, repository = $4, archived = $5 WHERE id = $6",
+      [req.body.title, req.body.description, req.body.website, req.body.repository, 
+        getDBArchivedValue(req), req.body.id])
+  }
+  req.flash('success', 'Successfully updated project!');
   res.redirect('/projects');
 });
 
