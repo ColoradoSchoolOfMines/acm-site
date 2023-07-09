a23f72801b997b1c6df62be74379a35090724277
Alexander Capehart <alex@oxycblt.org>
2023-07-09 15:56:08 -0600

    Add project editing to backend
    And also handle invalid author email configurations.
    
    Couldn't really split this commit good.

--- routes/projects.js
+++ routes/projects.js
@@ -5,9 +5,24 @@ const uuid = require('uuid');
 const { isAdminAuthenticated, upload } = require('../middleware');
 const router = express.Router();
 
-const getAuthorValue = (req, i) => req.body[`author${i}`]
+// Methods to help parse project queries
 
-const getArchivedValue = (req) => (req.body.archived !== undefined).toString()
+const getAuthors = (req) => {
+  const getAuthorValue = (req, i) => req.body[`author${i}`]
+
+  let emails = [];
+  for (let i = 1; getAuthorValue(req, i); ++i) {
+    const email = getAuthorValue(req, i);
+    if (!email.endsWith("@mines.edu")) {
+      return null;
+    }
+    emails.push(email);
+  }
+
+  return emails;
+}
+
+const getArchived = (req) => (req.body.archived !== undefined).toString()
 
 const clearProjectImage = async (req) => {
   // We don't have access to the image id from the request usually, query it from the db.
@@ -29,46 +44,67 @@ router.get('/projects', async (req, res) => {
 });
 
 router.post('/projects', isAdminAuthenticated, upload('image'), async (req, res) => {
-  let id = uuid.v4();
-  await db.query(
-    "INSERT INTO projects VALUES ($1, $2, $3, $4, $5, $6, $7)",
-    [id, req.body.title, req.body.description, req.body.website, req.body.repository, 
-      getArchivedValue(req), req.file.filename]);
-  let i = 1;
-  while (getAuthorValue(req, i)) {
-    let email = getAuthorValue(req, i);
+  // Must validate authors here to avoid partially writing projects
+  let authors = getAuthors(req);
+  if (authors) {
+    let id = uuid.v4();
     await db.query(
-      "INSERT INTO user_projects (user_id, project_id) VALUES ($1, $2)", [email, id])
-    ++i;
+      "INSERT INTO projects VALUES ($1, $2, $3, $4, $5, $6, $7)",
+      [id, req.body.title, req.body.description, req.body.website, req.body.repository, 
+        getArchived(req), req.file.filename]);
+
+    for (let author of authors) {
+      await db.query(
+        "INSERT INTO user_projects VALUES ($1, $2)", [author, id])
+    } 
+
+    req.flash('success', 'Successfully added project!');
+  } else {
+    req.flash('error', "Project authors can only have @mines.edu addresses!");
   }
-  req.flash('success', 'Successfully added project!');
+
   res.redirect('/projects');
 });
 
 router.post('/projects/edit', isAdminAuthenticated, upload('image', true), async (req, res) => {
-  // Image uploading is optional when editing projects, so we need to perform different queries
-  // for the different states.
-  if (req.file) {
-    // Free the space taken up by the now-unused project image.
-    await clearProjectImage(req);
-    await db.query(
-      "UPDATE projects SET title = $1, description = $2, website = $3, repository = $4, archived = $5, image_id = $6 WHERE id = $7",
-      [req.body.title, req.body.description, req.body.website, req.body.repository, 
-        getArchivedValue(req), req.file.filename, req.body.id])
+  // Must validate here to avoid partially writing project edits even with invalid authors
+  let authors = getAuthors(req);
+  if (authors) {
+    // Image uploading is optional when editing projects, so we need to perform different queries
+    // for the different states.
+    if (req.file) {
+      // Free the space taken up by the now-unused project image.
+      await clearProjectImage(req);
+      await db.query(
+        "UPDATE projects SET title = $1, description = $2, website = $3, repository = $4, archived = $5, image_id = $6 WHERE id = $7",
+        [req.body.title, req.body.description, req.body.website, req.body.repository, 
+          getArchivedValue(req), req.file.filename, req.body.id])
+    } else {
+      // No image specified, leave it unchanged and only commit the rest.
+      await db.query(
+        "UPDATE projects SET title = $1, description = $2, website = $3, repository = $4, archived = $5 WHERE id = $6",
+        [req.body.title, req.body.description, req.body.website, req.body.repository, 
+          getArchived(req), req.body.id])
+    }
+
+    await db.query("DELETE FROM user_projects WHERE project_id = $1", [req.body.id]);
+    for (let author of authors) {
+      await db.query(
+        "INSERT INTO user_projects VALUES ($1, $2)", [author, req.body.id])
+    } 
+
+    req.flash('success', 'Successfully updated project!');
   } else {
-    // No image specified, leave it unchanged and only commit the rest.
-    await db.query(
-      "UPDATE projects SET title = $1, description = $2, website = $3, repository = $4, archived = $5 WHERE id = $6",
-      [req.body.title, req.body.description, req.body.website, req.body.repository, 
-        getArchivedValue(req), req.body.id])
+    req.flash('error', "Project authors can only have @mines.edu addresses!");
   }
-  req.flash('success', 'Successfully updated project!');
+
   res.redirect('/projects');
 });
 
 router.post('/projects/delete', isAdminAuthenticated, async (req, res) => {
   // Free the space taken up by the now-unused project image.
   await clearProjectImage(req);
+  await db.query("DELETE FROM user_projects WHERE project_id = $1", [req.body.id]);
   await db.query("DELETE FROM projects WHERE id = $1", [req.body.id]);
   req.flash('success', 'Successfully deleted project!');
   res.redirect('/projects');
