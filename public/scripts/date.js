96403a8e4d4f298c1a1611dca6eaa2ff496ec50a
Alexander Capehart <alex@oxycblt.org>
2023-07-12 13:49:34 -0600

    Add short date class
    Split the "date" class into two new classes:
    - "date" outputs a plain date like "July 12th, 2023"
    - "datetime" preserves the old behavior of outputting a full
    date and duration
    
    Date will be used for presentations, while datetime is used for
    meetings.

--- public/scripts/date.js
+++ public/scripts/date.js
@@ -1,12 +1,23 @@
 let dates = document.getElementsByClassName("date");
-for (let i = 0; i < dates.length; i++) {
-	const date = new Date(dates[i].innerHTML.split(",")[0]);
-	const duration = dates[i].innerHTML.split(",")[1];
-	const endTime = new Date(date.getTime() + parseInt(duration));
+for (let timestamp of dates) {
+	const date = new Date(timestamp.innerHTML);
+	timestamp.innerHTML = new Date(date).toLocaleDateString('en-US', {
+		year: 'numeric',
+		month: 'long',
+		day: 'numeric',
+	});
+}
+
+let dateTimes = document.getElementsByClassName("datetime");
+for (let timestamp of dateTimes) {
+	const split = timestamp.innerHTML.split(",");
+	const date = new Date(split[0]);
+	const duration = parseInt(split[1]);
+	const endTime = new Date(date.getTime() + duration);
 	const newDuration = " from " + date.toLocaleDateString('en-US', { hour: 'numeric' }).split(", ")[1]
 		+ " to " + endTime.toLocaleDateString('en-US', { hour: 'numeric' }).split(", ")[1];
 
-	dates[i].innerHTML = new Date(date).toLocaleDateString('en-US', {
+	timestamp.innerHTML = date.toLocaleDateString('en-US', {
 		weekday: 'long',
 		year: 'numeric',
 		month: 'long',
