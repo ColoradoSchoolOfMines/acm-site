1eaf2a8586335d9b0e8cef94c274647f13c39508
Ethan Richards <42894274+ezrichards@users.noreply.github.com>
2023-07-15 16:31:44 -0600

    Fix officer edit edge case

--- routes/admin.js
+++ routes/admin.js
@@ -23,15 +23,16 @@ router.post('/admin', isAdminAuthenticated, upload('image'), async (req, res) =>
 });
 
 router.post('/officers', isAdminAuthenticated, async (req, res) => {
-  await db.query("UPDATE users SET title = $1 WHERE email = $2", [req.body.title, req.body.email]);
-  req.flash('success', 'Successfully added officer role for ' + req.body.email + '.');
-  res.redirect('/admin');
-});
-
-router.post('/officers/edit', isAdminAuthenticated, async (req, res) => {
-  await db.query("UPDATE users SET title = $1 WHERE email = $2", [req.body.title, req.body.email]);
-  req.flash('success', 'Successfully edited officer ' + req.body.email + '.');
-  res.redirect('/admin');
+  const resp = await db.query("SELECT 1 FROM users WHERE email = $1", [req.body.email]);
+  if(resp.rows.length > 0) {
+    await db.query("UPDATE users SET title = $1 WHERE email = $2", [req.body.title, req.body.email]);
+    req.flash('success', 'Successfully set officer role to ' + req.body.title + ' for ' + req.body.email + '.');
+    res.redirect('/admin');
+  }
+  else {
+    req.flash('error', 'There is not a user with that email!');
+    res.redirect('/admin');
+  }
 });
 
 router.post('/officers/remove', isAdminAuthenticated, async (req, res) => {
