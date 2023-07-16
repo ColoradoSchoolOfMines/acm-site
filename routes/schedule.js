90fc240a454ab085fcd2d1b92ff76290aff2b357
Alexander Capehart <alex@oxycblt.org>
2023-07-16 15:34:59 -0600

    Add ical route
    Add an ical-file generating route to /schedules leveraging
    ical-generator.

--- routes/schedule.js
+++ routes/schedule.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const db = require('../database/db');
+const ical = require('ical-generator');
 const router = express.Router();
 
 router.get('/schedule', async (req, res) => {
@@ -18,6 +19,34 @@ router.get('/schedule', async (req, res) => {
 
   let previous = await db.query("SELECT * FROM meetings WHERE date <= NOW() ORDER BY date DESC");
   res.render('schedule', { title: 'Schedule', upcoming: upcoming.rows, previous: previous.rows });
+});
+
+router.get('/schedule/ical.ics', async (req, res) => {
+  const resp = await db.query("SELECT * FROM meetings");
+
+  const calendar = ical.default({ 
+    name: 'Mines ACM',
+    description: "All of the talks\\, open source meetings\\, and other activities of the Mines ACM Student Chapter.",
+    prodId: {
+      company: 'Mines ACM',
+      product: 'web',
+      language: 'EN'
+    }
+  });
+
+  for (let meeting of resp.rows) {
+    calendar.createEvent({
+      id: meeting.id,
+      summary: meeting.title,
+      description: meeting.description,
+      location: meeting.location,
+      start: meeting.date,
+      timezone: 'America/Denver',
+      end: new Date(meeting.date.getTime() + meeting.duration)
+    })
+  }
+
+  calendar.serve(res);
 });
 
 module.exports = router;
