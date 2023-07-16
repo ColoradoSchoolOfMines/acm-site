fbdfba06abddf9564476de79b667862b7c682d1c
Alexander Capehart <alex@oxycblt.org>
2023-07-16 13:53:37 -0600

    Move schedule routes into module
    More schedule routes are going to be added, so better to do this.

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const adminRoutes = require('./routes/admin');
 const profileRoutes = require('./routes/profile');
 const projectsRoutes = require('./routes/projects');
 const presentationsRoutes = require('./routes/presentations');
+const scheduleRoutes = require('./routes/schedule');
 const app = express();
 
 app.engine('ejs', ejsMate);
@@ -93,6 +94,7 @@ app.use('/', authRoutes);
 app.use('/', presentationsRoutes);
 app.use('/', profileRoutes);
 app.use('/', projectsRoutes);
+app.use('/', scheduleRoutes);
 
 app.get('/', async (req, res) => {
   const resp = await db.query("SELECT * FROM images ORDER BY RANDOM() LIMIT 1");
@@ -117,24 +119,6 @@ app.get('/', async (req, res) => {
 app.get('/about', async (req, res) => {
   const resp = await db.query("SELECT * FROM users WHERE title != ''");
   res.render('about', { title: 'About Us', people: resp.rows });
-});
-
-app.get('/schedule', async (req, res) => {
-  let upcoming = await db.query("SELECT * FROM meetings WHERE date >= NOW() AND date <= NOW() + INTERVAL '3 weeks' ORDER BY date");
-  for (let meeting in upcoming.rows) {
-    if (req.user) {
-      const rsvp = await db.query("SELECT * FROM rsvps WHERE email = $1 AND meeting = $2", [req.user.email, upcoming.rows[meeting].id]);
-      if (rsvp.rows.length > 0) {
-        upcoming.rows[meeting].rsvped = true;
-      }
-    }
-    else {
-      upcoming.rows[meeting].rsvped = false;
-    }
-  }
-
-  let previous = await db.query("SELECT * FROM meetings WHERE date <= NOW() ORDER BY date DESC");
-  res.render('schedule', { title: 'Schedule', upcoming: upcoming.rows, previous: previous.rows });
 });
 
 app.get('/uploads/:id', (req, res) => {
