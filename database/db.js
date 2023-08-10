4d6f496d2d3cf4c30f35d4fd0a014d3d5fd5a9ab
Alexander Capehart <alex@oxycblt.org>
2023-08-10 17:02:35 -0600

    Introduce DB transactions
    Will be used w/more general error handling to prevent partial DB writes.

--- database/db.js
+++ database/db.js
@@ -3,6 +3,19 @@ const pg = require('pg');
 const pool = new pg.Pool({ connectionString: process.env.DB_URL });
 
 module.exports = {
+    transaction: async (block) => {
+        const client = await pool.connect();
+        try {
+            await client.query("BEGIN");
+            await block(client);
+            await client.query("COMMIT");
+        } catch (e) {
+            await client.query("ROLLBACK");
+            throw e;
+        } finally {
+            client.release();
+        }
+    },
     query: async (text, params, callback) => {
         return pool.query(text, params, callback);
     },
