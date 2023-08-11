52ac6c5b52f3ac836aa3461824584ca8dce58378
Alexander Capehart <alex@oxycblt.org>
2023-08-11 17:04:00 -0600

    Improve auth error handling

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,13 @@
 const passport = require('passport');
 const express = require('express');
 const router = express.Router();
+const { fallible } = require('../middleware')
 
-router.get('/login', passport.authenticate('google', { scope: ['email', 'profile'] }), (req, res) => {
+router.get('/login', passport.authenticate('google', { scope: ['email', 'profile'] }), fallible(async (req, res) => {
   res.redirect('/');
-});
+}));
 
-router.get('/logout', (req, res) => {
+router.get('/logout', fallible(async (req, res) => {
   req.logout((err) => {
     if (err) {
       req.flash('error', 'Could not successfully log out! Please contact a site administrator.')
@@ -17,11 +18,11 @@ router.get('/logout', (req, res) => {
       res.redirect('/');
     }
   });
-});
+}));
 
-router.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/', keepSessionInfo: true }), (req, res) => {
+router.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/', keepSessionInfo: true }), fallible(async (req, res) => {
   res.redirect(req.session.returnTo || '/');
   delete req.session.returnTo;
-});
+}));
 
 module.exports = router;
