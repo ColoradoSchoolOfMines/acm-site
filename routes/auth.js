37b8bb7950c13cf05d98be3495f9bbc4a1465ea3
Alexander Capehart <alex@oxycblt.org>
2023-08-13 08:46:18 -0600

    Remove auth fallible usage

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,12 @@
 const passport = require('passport');
 const express = require('express');
 const router = express.Router();
-const { fallible } = require('../middleware')
 
-router.get('/login', passport.authenticate('google', { scope: ['email', 'profile'] }), fallible(async (req, res) => {
+router.get('/login', passport.authenticate('google', { scope: ['email', 'profile'] }), async (req, res) => {
   res.redirect('/');
-}));
+});
 
-router.get('/logout', fallible(async (req, res) => {
+router.get('/logout', async (req, res) => {
   req.logout((err) => {
     if (err) {
       req.flash('error', 'Could not successfully log out! Please contact a site administrator.')
@@ -18,11 +17,11 @@ router.get('/logout', fallible(async (req, res) => {
       res.redirect('/');
     }
   });
-}));
+});
 
-router.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/', keepSessionInfo: true }), fallible(async (req, res) => {
+router.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/', keepSessionInfo: true }), async (req, res) => {
   res.redirect(req.session.returnTo || '/');
   delete req.session.returnTo;
-}));
+});
 
 module.exports = router;
